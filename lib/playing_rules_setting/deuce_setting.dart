0ebaddc943e09ace6713634b2292baa468cbda90
wayne <wayne8692wayne8692@gmail.com>
2022-07-20 21:32:28 +0800

    資料結構設計初版，新增Provider套件來搭配Stateful做表單更新

---
+++ lib/playing_rules_setting/deuce_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeuceSetting extends StatefulWidget {
+  const DeuceSetting({Key? key}) : super(key: key);
+
+  @override
+  State<DeuceSetting> createState() => _DeuceSettingState();
+}
+
+class _DeuceSettingState extends State<DeuceSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
